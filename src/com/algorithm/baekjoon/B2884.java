bc75bfdd2c036822fe4afc7949aa5950dcba19b8
IGOR <132262b@naver.com>
2021-12-31 23:04:35 +0900

    백준 / 2884 / 알람 시계

---
+++ src/com/algorithm/baekjoon/B2884.java
@@ -0,0 +1,30 @@
+package com.algorithm.baekjoon;
+
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/2884"/> 문제주소 </a>
+ * 백준 / 2884 / 알람 시계
+ *
+ */
+public class B2884 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int hh = sc.nextInt();
+        int mm = sc.nextInt();
+
+        mm -= 45;
+        if(mm < 0) {
+            mm += 60;
+            hh -= 1;
+            if(hh < 0)
+                hh +=24;
+        }
+
+        System.out.println(hh+" "+mm);
+
+    }
+}
