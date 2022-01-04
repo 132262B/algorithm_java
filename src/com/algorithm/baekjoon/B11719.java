9d1f340c47d6a83516d1541b71ecbe5efcdc6032
IGOR <132262b@naver.com>
2022-01-04 21:35:01 +0900

    백준 / 11719 / 그대로 출력하기 2

---
+++ src/com/algorithm/baekjoon/B11719.java
@@ -0,0 +1,19 @@
+package com.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/11719"/> 문제주소 </a>
+ * 백준 / 11719 / 그대로 출력하기 2
+ */
+public class B11719 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+        sc.close();
+    }
+}
