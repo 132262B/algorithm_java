ba3f1fa9e9bf328133083a5c5ea18a7a454d99ac
IGOR <132262b@naver.com>
2022-01-06 01:04:33 +0900

    백준 / 1427 / 소트인사이드

---
+++ src/com/algorithm/baekjoon/silver/B1427.java
@@ -0,0 +1,30 @@
+package com.algorithm.baekjoon.silver;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/1427"/> 문제주소 </a>
+ * 백준 / 1427 / 소트인사이드
+ */
+public class B1427 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            list.add(String.valueOf(str.charAt(i)));
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        for (String j : list) {
+            System.out.print(j);
+        }
+    }
+}
