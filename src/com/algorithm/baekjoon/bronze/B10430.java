fb435f0f655044bda51d9592c7dc79b5ee528b39
IGOR <132262b@naver.com>
2022-01-06 00:36:35 +0900

    백준 / 10430 / 나머지

---
+++ src/com/algorithm/baekjoon/bronze/B10430.java
@@ -0,0 +1,24 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/10430"/> 문제주소 </a>
+ * 백준 / 10430 / 나머지
+ */
+public class B10430 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+
+    }
+}
