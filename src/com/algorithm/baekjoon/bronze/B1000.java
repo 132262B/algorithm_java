d3b6d976554917f2d75d13bcc6c7763500e56ae7
IGOR <132262b@naver.com>
2022-01-20 19:23:42 +0900

    백준 / 1000 / A+B

---
+++ src/com/algorithm/baekjoon/bronze/B1000.java
@@ -0,0 +1,16 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/1000"/> 문제주소 </a>
+ * 백준 / 1000 / A+B
+ */
+public class B1000 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()+sc.nextInt());
+    }
+
+}
