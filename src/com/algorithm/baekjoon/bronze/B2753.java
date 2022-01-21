d586984f6d19e051019eb7fd56e182023cc7cd61
IGOR <132262b@naver.com>
2022-01-21 22:34:02 +0900

    백준 / 2753 / 윤년

---
+++ src/com/algorithm/baekjoon/bronze/B2753.java
@@ -0,0 +1,26 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/2753"/> 문제주소 </a>
+ * 백준 / 2753 / 윤년
+ */
+public class B2753 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        sc.close();
+
+        if (num % 4 == 0) {
+
+            if (num % 400 == 0) System.out.println("1");
+            else if (num % 100 == 0) System.out.println("0");
+            else System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
