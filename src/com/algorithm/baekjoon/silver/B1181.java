475f27a41300b2afc3f1e1bbd5dba6da80da0ded
IGOR <132262b@naver.com>
2022-01-26 19:53:16 +0900

    백준 / 11650 / 좌표 정렬하기

---
+++ src/com/algorithm/baekjoon/silver/B1181.java
@@ -0,0 +1,22 @@
+package com.algorithm.baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class B1181 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        ArrayList<String> str = new ArrayList<>();
+
+
+    }
+}
