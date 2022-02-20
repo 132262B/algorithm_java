66036c8252b0721560da27fdac00e7124cb6421d
IGOR <132262b@naver.com>
2022-02-21 00:37:23 +0900

    백준 / 2558 / A+B - 2

---
+++ src/com/algorithm/boj/bronze/B2558.java
@@ -0,0 +1,18 @@
+package com.algorithm.boj.bronze;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/2558"/> 문제주소 </a>
+ * 백준 / 2558 / A+B - 2
+ */
+public class B2558 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
