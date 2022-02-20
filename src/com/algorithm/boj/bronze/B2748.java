14748c82ef3b686c406bdb4991c8bff2695b352c
IGOR <132262b@naver.com>
2022-02-20 14:57:33 +0900

    백준 / 2748 / 피보나치 수 2

---
+++ src/com/algorithm/boj/bronze/B2748.java
@@ -0,0 +1,30 @@
+package com.algorithm.boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/2748"/> 문제주소 </a>
+ * 백준 / 2748 / 피보나치 수 2
+ */
+public class B2748 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()) + 1;
+
+        long[] arr = new long[n];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+
+        System.out.println(arr[n - 1]);
+
+    }
+}
