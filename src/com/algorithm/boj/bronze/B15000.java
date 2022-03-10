65f8eda4f4c493eb0325c5ae828dbf10b076b941
IGOR <132262b@naver.com>
2022-03-10 23:28:55 +0900

    BOJ / 15000 / CAPS

---
+++ src/com/algorithm/boj/bronze/B15000.java
@@ -0,0 +1,18 @@
+package com.algorithm.boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/15000"/> 문제주소 </a>
+ * BOJ / 15000 / CAPS
+ */
+public class B15000 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str.toUpperCase());
+    }
+}
